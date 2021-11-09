3574a3392ac610dc807225b2057ca37f552307ba
Mervyn Mathew <mm2373@njit.edu>
2021-11-08 22:37:18 -0500

    Pressing exit game button exits the game

---
+++ Japanese_Summer_Festival/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    // Exit
+    public void ExitApp() {
+        Application.Quit();
+    }
+}
