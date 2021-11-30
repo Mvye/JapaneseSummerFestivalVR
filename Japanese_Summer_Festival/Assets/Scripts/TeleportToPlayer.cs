c41c0efb7cdf52f36c8d0715d026e1ec0e06fde8
David Garcia <dg533@njit.edu>
2021-11-30 05:25:03 -0500

    Final touches

--- Japanese_Summer_Festival/Assets/Scripts/TeleportToPlayer.cs
+++ Japanese_Summer_Festival/Assets/Scripts/TeleportToPlayer.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class TeleportToPlayer : MonoBehaviour
 {
     public Transform teleportPoint;
-    Transform _transform;
 
     void Start()
     {
-        _transform = this.transform;
+
     }
-    void onCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Teleport"))
         {
             Debug.Log("Colliding with the teleport object");
-            _transform.transform.position = teleportPoint.transform.position;
+            this.gameObject.transform.position = teleportPoint.transform.position;
         }
     }
 }
